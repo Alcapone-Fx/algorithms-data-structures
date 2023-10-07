bd442b77b32646d1981828816c2df05c24138c99
Felix Navas <navasfx19@gmail.com>
2023-10-07 10:04:22 -0600

    ♻️Files capitalized

---
+++ src/strings/tests/Concatenation.test.ts
@@ -0,0 +1,11 @@
+import { concatenate } from '../Concatenation';
+
+describe('concatenate', () => {
+  it('Should return empty string', () => {
+    expect(concatenate('', '')).toBe('');
+  });
+
+  it('Should return the concatenation of 2 strings', () => {
+    expect(concatenate('abc ', 'def')).toBe('abc def');
+  });
+});
