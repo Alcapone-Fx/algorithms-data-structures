13319e69491890a087f8e569f949c53464d39114
Felix Navas <navasfx19@gmail.com>
2023-10-11 14:41:05 -0600

    ✨String: LeftRotate, RightRotate

--- src/strings/Rotate.ts
+++ src/strings/Rotate.ts
@@ -1,7 +1,6 @@
 /**
  * Given a string, return an array with all possible rotations
  */
-
 export const rotate = (str: string) => {
   const rotations: string[] = [];
   if (!str) {
@@ -17,4 +16,67 @@ export const rotate = (str: string) => {
   }
 
   return rotations;
+};
+
+/**
+ * Right rotate the given string by d elements
+ */
+export const rightRotate = (str: string, rotate: number) => {
+  if (!str || rotate <= 0) {
+    return str;
+  }
+
+  const repeated = str + str;
+  const strLength = str.length;
+  const offset = rotate % strLength || strLength;
+
+  return repeated.slice(-strLength - offset, -offset);
+};
+
+/**
+ * Left rotate the given string by d elements
+ */
+export const leftRotate = (str: string, rotate: number) => {
+  if (!str || rotate <= 0) {
+    return str;
+  }
+
+  const repeated = str + str;
+  const strLength = str.length;
+  const offset = rotate % strLength || strLength;
+
+  return repeated.slice(offset, strLength + offset);
+};
+
+/**
+ * Left rotate the given string by d elements
+ * Note: best performance
+ */
+export const leftRotate1 = (str: string, rotate: number) => {
+  if (!str || rotate <= 0) {
+    return str;
+  }
+
+  rotate = rotate % str.length; // Handle cases where d is greater than the string length
+  const leftPart = str.slice(rotate);
+  const rightPart = str.slice(0, rotate);
+
+  return leftPart + rightPart;
+};
+
+/**
+ * Right rotate the given string by d elements
+ * Note: best performance
+ */
+export const rightRotate1 = (str: string, rotate: number) => {
+  if (!str || rotate <= 0) {
+    return str;
+  }
+
+  const n = str.length;
+  rotate = rotate % n; // Handle cases where d is greater than the string length
+  const leftPart = str.slice(n - rotate);
+  const rightPart = str.slice(0, n - rotate);
+
+  return leftPart + rightPart;
 };
--- src/strings/tests/Rotate.test.ts
+++ src/strings/tests/Rotate.test.ts
@@ -1,4 +1,4 @@
-import { rotate } from '../Rotate';
+import { rotate, rightRotate, rightRotate1, leftRotate, leftRotate1 } from '../Rotate';
 
 describe('rotate', () => {
   it('should return an array with one element when given a string with length 1', () => {
@@ -25,3 +25,134 @@ describe('rotate', () => {
     expect(rotate('')).toEqual([]);
   });
 });
+
+describe('rightRotate', () => {
+  it('should return the original string when rotate is 0', () => {
+    const str = 'hello';
+    const rotate = 0;
+    const result = rightRotate(str, rotate);
+    expect(result).toBe(str);
+  });
+
+  it('should return the original string when rotate is a multiple of string length', () => {
+    const str = 'hello';
+    const rotate = 10;
+    const result = rightRotate(str, rotate);
+    expect(result).toBe(str);
+  });
+
+  it('should return the expected rotated string when rotate is less than string length', () => {
+    const str = 'hello';
+    const rotate = 2;
+    const result = rightRotate(str, rotate);
+    expect(result).toBe('lohel');
+  });
+
+  it('should return an empty string when given an empty string', () => {
+    const str = '';
+    const rotate = 3;
+    const result = rightRotate(str, rotate);
+    expect(result).toBe('');
+  });
+
+  it('should return the original string when given a negative rotate value', () => {
+    const str = 'hello';
+    const rotate = -2;
+    const result = rightRotate(str, rotate);
+    expect(result).toBe(str);
+  });
+
+  it('should return correct value when given a rotate value greater than 2 times the string length', () => {
+    const str = 'hello';
+    const rotate = 12;
+    const result = rightRotate(str, rotate);
+    expect(result).toBe('lohel');
+  });
+});
+
+describe('rightRotate1', () => {
+  it('should return the original string when rotate is 0', () => {
+    const str = 'hello';
+    const rotate = 0;
+    const result = rightRotate1(str, rotate);
+    expect(result).toBe(str);
+  });
+
+  it('should return the original string when rotate is a multiple of string length', () => {
+    const str = 'hello';
+    const rotate = 10;
+    const result = rightRotate1(str, rotate);
+    expect(result).toBe(str);
+  });
+
+  it('should return the expected rotated string when rotate is less than string length', () => {
+    const str = 'hello';
+    const rotate = 2;
+    const result = rightRotate1(str, rotate);
+    expect(result).toBe('lohel');
+  });
+
+  it('should return an empty string when given an empty string', () => {
+    const str = '';
+    const rotate = 3;
+    const result = rightRotate1(str, rotate);
+    expect(result).toBe('');
+  });
+
+  it('should return the original string when given a negative rotate value', () => {
+    const str = 'hello';
+    const rotate = -2;
+    const result = rightRotate1(str, rotate);
+    expect(result).toBe(str);
+  });
+
+  it('should return correct value when given a rotate value greater than 2 times the string length', () => {
+    const str = 'hello';
+    const rotate = 12;
+    const result = rightRotate1(str, rotate);
+    expect(result).toBe('lohel');
+  });
+});
+
+describe('leftRotate', () => {
+  it('should rotate string by 1 element', () => {
+    const str = 'hello';
+    const rotate = 1;
+    const expected = 'elloh';
+    const result = leftRotate(str, rotate);
+    expect(result).toEqual(expected);
+  });
+
+  it('should rotate string by length of string - 1 elements', () => {
+    const str = 'hello';
+    const rotate = str.length - 1;
+    const expected = 'ohell';
+    const result = leftRotate(str, rotate);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return empty string when rotating empty string', () => {
+    const str = '';
+    const rotate = 2;
+    const expected = '';
+    const result = leftRotate(str, rotate);
+    expect(result).toEqual(expected);
+  });
+
+  it('should rotate string by length of string + 1 elements', () => {
+    const str = 'hello';
+    const rotate = str.length + 1;
+    const expected = 'elloh';
+    const result = leftRotate(str, rotate);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return original string when rotating by negative number of elements', () => {
+    const str = 'hello';
+    const rotate = -2;
+    const expected = 'hello';
+    const result = leftRotate(str, rotate);
+    expect(result).toEqual(expected);
+  });
+});
+
